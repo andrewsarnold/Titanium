e91467616b70f638591ff72db760ad1d3dd0c4ba
Andrew Arnold <andrew.s.arnold@gmail.com>
2016-02-03 09:59:11 -0500

    Refactoring unit tests

---
+++ Titanium.Test/EvaluateTests/Common.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Titanium.Core.Expressions;
+
+namespace Titanium.Test.EvaluateTests
+{
+	internal static class Common
+	{
+		internal static void EvaluateAndAssert(string input, string output)
+		{
+			var expression = Expression.ParseExpression(input);
+			var result = expression.Evaluate();
+			Assert.AreEqual(output, result.ToString());
+		}
+
+		internal static void AssertThrows(string input)
+		{
+			try
+			{
+				EvaluateAndAssert(input, string.Empty);
+				Assert.Fail();
+			}
+			catch (Exception)
+			{
+				Assert.IsTrue(true);
+			}
+		}
+	}
+}
---
+++ Titanium.Test/EvaluateTests/TrigonometryTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Titanium.Test.EvaluateTests
+{
+	[TestClass]
+	public class TrigonometryTests
+	{
+		[TestMethod]
+		public void EvaluateSinTest()
+		{
+			Common.EvaluateAndAssert("sin(1.3)", "0.963558185417193");
+			Common.EvaluateAndAssert("sin(⁻1.3)", "⁻0.963558185417193");
+			Common.EvaluateAndAssert("sin(0)", "0");
+			Common.EvaluateAndAssert("sin(3)", "sin(3)");
+			Common.EvaluateAndAssert("sin(π)", "0");
+			Common.EvaluateAndAssert("sin(e)", "sin(e)");
+		}
+
+		[TestMethod]
+		public void EvaluateCosTest()
+		{
+			Common.EvaluateAndAssert("cos(1.3)", "0.267498828624587");
+			Common.EvaluateAndAssert("cos(⁻1.3)", "0.267498828624587");
+			Common.EvaluateAndAssert("cos(0)", "1");
+			Common.EvaluateAndAssert("cos(3)", "cos(3)");
+			Common.EvaluateAndAssert("cos(π)", "⁻1");
+		}
+
+		[TestMethod]
+		public void EvaluateTanTest()
+		{
+			Common.EvaluateAndAssert("tan(1.3)", "3.60210244796798");
+			Common.EvaluateAndAssert("tan(⁻1.3)", "⁻3.60210244796798");
+			Common.EvaluateAndAssert("tan(0)", "0");
+			// todo: tan pi / 2
+		}
+	}
+}
