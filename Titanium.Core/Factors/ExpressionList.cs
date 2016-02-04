de44bd006ec40ab45191edad19e8f07fb314b056
Andrew Arnold <andrew.s.arnold@gmail.com>
2016-02-04 13:26:01 -0500

    ExpressionList type

---
+++ Titanium.Core/Factors/ExpressionList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Titanium.Core.Expressions;
+using Titanium.Core.Reducer;
+
+namespace Titanium.Core.Factors
+{
+	internal class ExpressionList : Factor
+	{
+		private readonly IEnumerable<Expression> _expressions;
+
+		public ExpressionList(IEnumerable<Expression> expressions)
+		{
+			_expressions = expressions;
+		}
+
+		public override Expression Evaluate()
+		{
+			return Expressionizer.ToExpression(this);
+		}
+
+		public override string ToString()
+		{
+			return string.Format("{{{0}}}", string.Join(",", _expressions));
+		}
+	}
+}
