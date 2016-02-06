30ca5f6a9f117ea6918e18b678495afe3b02f744
Andrew Arnold <andrew.s.arnold@gmail.com>
2016-02-06 11:58:35 -0500

    Fixing evaluation of integer fraction in an expression

--- Titanium.Test/EvaluateTests/ArithmeticTests.cs
+++ Titanium.Test/EvaluateTests/ArithmeticTests.cs
@@ -69,12 +69,13 @@ namespace Titanium.Test.EvaluateTests
 		{
 			Common.EvaluateAndAssert("(12 - 3) + 5 / 4", "41/4");
 			Common.EvaluateAndAssert("(12 - 3) + ((5 * 4)-1)-2", "26");
-
 			Common.EvaluateAndAssert("3/1", "3");
 			Common.EvaluateAndAssert("4+3/1", "7");
 			Common.EvaluateAndAssert("3/1+2", "5");
 			Common.EvaluateAndAssert("4+3/1+2", "9");
 			Common.EvaluateAndAssert("4+(3/1)+2", "9");
+			Common.EvaluateAndAssert("3*4/3", "4");
+			Common.EvaluateAndAssert("3*(4/3)", "4");
 		}
 
 		[TestMethod]
