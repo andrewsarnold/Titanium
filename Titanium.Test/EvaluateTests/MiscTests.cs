d742c6a23e7326e6aa72ab8192238c7749c4e760
Andrew Arnold <andrew.s.arnold@gmail.com>
2016-02-06 19:15:31 -0500

    Breaking out factorial tests (with lots of failing ones)

--- Titanium.Test/EvaluateTests/MiscTests.cs
+++ Titanium.Test/EvaluateTests/MiscTests.cs
@@ -11,16 +11,5 @@ namespace Titanium.Test.EvaluateTests
 			Common.EvaluateAndAssert("(12 - a)", "12-a");
 			Common.EvaluateAndAssert("a * b -3", "a*b-3");
 		}
-
-		[TestMethod]
-		public void EvaluateFactorialTest()
-		{
-			Common.EvaluateAndAssert("⁻10!", "1");
-			Common.EvaluateAndAssert("0!", "1");
-			Common.EvaluateAndAssert("1!", "1");
-			Common.EvaluateAndAssert("3!", "6");
-			Common.EvaluateAndAssert("4!", "24");
-			Common.EvaluateAndAssert("2.5!", "(2.5)!");
-		}
 	}
 }
