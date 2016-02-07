d0f57066910239b1881083067d28938da1fe9b1e
Andrew Arnold <andrew.s.arnold@gmail.com>
2016-02-06 22:37:54 -0500

    Arithmetic and implied multiplication

--- Titanium.Test/EvaluateTests/ComplexArithmeticTests.cs
+++ Titanium.Test/EvaluateTests/ComplexArithmeticTests.cs
@@ -3,7 +3,7 @@
 namespace Titanium.Test.EvaluateTests
 {
 	[TestClass]
-	public class ArithmeticTests
+	public class ComplexArithmeticTests
 	{
 		[TestMethod]
 		public void EvaluateComplexTest()
@@ -11,22 +11,11 @@ namespace Titanium.Test.EvaluateTests
 			Common.EvaluateAndAssert("(12 - 3) + 5 / 4", "41/4");
 			Common.EvaluateAndAssert("(12 - 3) + ((5 * 4)-1)-2", "26");
 			Common.EvaluateAndAssert("3/1", "3");
-			Common.EvaluateAndAssert("4+3/1", "7");
 			Common.EvaluateAndAssert("3/1+2", "5");
 			Common.EvaluateAndAssert("4+3/1+2", "9");
 			Common.EvaluateAndAssert("4+(3/1)+2", "9");
 			Common.EvaluateAndAssert("3*4/3", "4");
 			Common.EvaluateAndAssert("3*(4/3)", "4");
-		}
-
-		[TestMethod]
-		public void EvaluateImpliedMultiplicationTest()
-		{
-			Common.EvaluateAndAssert("2(3)", "6");
-			Common.EvaluateAndAssert("(2)3", "6");
-			Common.EvaluateAndAssert("(3)(2)", "6");
-			Common.EvaluateAndAssert("2cos(1)", "2*cos(1)");
-			Common.EvaluateAndAssert("2cos(0)", "2");
 		}
 	}
 }
