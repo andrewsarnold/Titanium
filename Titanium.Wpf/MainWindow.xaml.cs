b1c4a7613bd654ed58567c7eaab385910f8cf07a
Andrew Arnold <andrew.s.arnold@gmail.com>
2016-02-09 10:51:05 -0500

    Basic functionality for a symbol menu

--- Titanium.Wpf/MainWindow.xaml.cs
+++ Titanium.Wpf/MainWindow.xaml.cs
@@ -1,10 +1,38 @@
-﻿namespace Titanium.Wpf
+﻿using System.Windows.Controls;
+
+namespace Titanium.Wpf
 {
 	public partial class MainWindow
 	{
 		public MainWindow()
 		{
 			InitializeComponent();
+			FillInsertMenu();
+			InputBox.Focus();
+		}
+
+		private void FillInsertMenu()
+		{
+			FillSymbolMenu();
+		}
+
+		private void FillSymbolMenu()
+		{
+			var menu = new MenuItem { Header = "Symbols" };
+			menu.Items.Add(SymbolMenuItem("⁻", "negative"));
+			MenuInsert.Items.Add(menu);
+		}
+
+		private MenuItem SymbolMenuItem(string symbol, string name)
+		{
+			var menuItem = new MenuItem { Header = string.Format("{0} ({1})", symbol, name) };
+			menuItem.Click += (sender, args) =>
+			{
+				InputBox.SelectedText = symbol;
+				InputBox.CaretIndex += symbol.Length;
+				InputBox.Focus();
+			};
+			return menuItem;
 		}
 	}
 }
