6cfbb5c08763b64c20051fc2236d6b3efceafa2c
Andrew Arnold <andrew.s.arnold@gmail.com>
2016-05-15 12:48:48 -0400

    Get result history with "ans()"

--- Titanium.Core/Evaluator.cs
+++ Titanium.Core/Evaluator.cs
@@ -1,22 +1,39 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using Titanium.Core.Expressions;
 
 namespace Titanium.Core
 {
 	public class Evaluator
 	{
-		private readonly Stack<Expression> _history;
+		private readonly List<Expression> _history;
 
 		public Evaluator()
 		{
-			_history = new Stack<Expression>();
+			_history = new List<Expression>();
 		}
 
 		public string Evaluate(string input)
 		{
+			input = ReplaceResultHistory(input);
 			var result = Expression.ParseExpression(input).Evaluate();
-			_history.Push(result);
+			_history.Insert(0, result);
 			return result.ToString();
+		}
+
+		private string ReplaceResultHistory(string input)
+		{
+			var matches = Regex.Matches(input, @"ans\((\d+)\)");
+
+			foreach (var match in matches.Cast<Match>())
+			{
+				var index = int.Parse(match.Groups[1].Value) - 1;
+				var result = string.Format("({0})", _history[index]);
+				input = input.Replace(match.Value, result);
+			}
+
+			return input;
 		}
 	}
 }
---
+++ Titanium.Test/StateTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Titanium.Core;
+
+namespace Titanium.Test
+{
+	[TestClass]
+	public class StateTests
+	{
+		[TestMethod]
+		public void HistoryTest()
+		{
+			var ev = new Evaluator();
+			ev.Evaluate("3.14");
+
+			var history = ev.Evaluate("ans(1)");
+			Assert.AreEqual("3.14", history);
+
+			ev.Evaluate("4.0");
+			ev.Evaluate("7.4");
+
+			history = ev.Evaluate("ans(2)");
+			Assert.AreEqual("4.", history);
+		}
+	}
+}
