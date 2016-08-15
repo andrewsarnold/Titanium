b0514494929b35ca6f4380c8a833a8e4586178c7
Andrew Arnold <andrew.s.arnold@gmail.com>
2016-08-15 11:40:35 -0400

    Interpret any number with scientific notation as a float

---
+++ Titanium.Test/EvaluateTests/ScientificNotationTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Titanium.Test.EvaluateTests
+{
+	[TestClass]
+	public class ScientificNotationTests
+	{
+		[TestMethod]
+		public void SmallIntegerIsExpanded()
+		{
+			Common.EvaluateAndAssert("1E4", "10000.");
+		}
+
+		[TestMethod]
+		public void SmallFloatIsExpanded()
+		{
+			Common.EvaluateAndAssert("1.E4", "10000.");
+		}
+	}
+}
