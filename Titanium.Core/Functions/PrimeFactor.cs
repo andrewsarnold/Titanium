a9f76efdb671e3ef31a08a6c6aedb187929a285f
Andrew Arnold <andrew.s.arnold@gmail.com>
2016-08-17 12:17:02 -0400

    Algorithm to calculate prime factors

--- Titanium.Core/Functions/PrimeFactor.cs
+++ Titanium.Core/Functions/PrimeFactor.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Titanium.Core.Components;
 using Titanium.Core.Expressions;
 using Titanium.Core.Factors;
+using Titanium.Core.Functions.Implementations;
+using Titanium.Core.Numbers;
 using Titanium.Core.Reducer;
 
 namespace Titanium.Core.Functions
@@ -17,9 +21,35 @@ namespace Titanium.Core.Functions
 		{
 			var parameter = parameters[0].Evaluate();
 			var asFactor = Factorizer.ToFactor(parameter);
-			if (asFactor is NumericFactor)
+			var numericFactor = asFactor as NumericFactor;
+			if (numericFactor != null)
 			{
-				return Expressionizer.ToExpression(asFactor);
+				var number = Math.Abs(numericFactor.Number.ValueAsFloat());
+				var plt = PrimesLessThan(number);
+				var primeCounts = new Dictionary<int, int>();
+				foreach (var prime in plt)
+				{
+					while (number % prime < Constants.Tolerance)
+					{
+						if (!primeCounts.ContainsKey(prime))
+						{
+							primeCounts.Add(prime, 1);
+						}
+						else
+						{
+							primeCounts[prime]++;
+						}
+
+						number /= prime;
+					}
+				}
+
+				var exponents = primeCounts.Select(pc => pc.Value > 1
+					? string.Format("{0}^{1}", pc.Key, pc.Value)
+					: pc.Key.ToString());
+				var expression = string.Join("*", exponents);
+				
+				return Expressionizer.ToExpression(numericFactor);
 			}
 
 			return parameter;
@@ -28,6 +58,20 @@ namespace Titanium.Core.Functions
 		internal override string ToString(List<Expression> parameters)
 		{
 			throw new NotImplementedException();
+		}
+
+		private static IEnumerable<int> PrimesLessThan(double value)
+		{
+			var max = (int)Math.Sqrt(value) + 1;
+			var sieve = Enumerable.Range(2, max).ToList();
+
+			for (var i = 0; i < sieve.Count; i++)
+			{
+				var thisPrime = sieve[i];
+				sieve.RemoveAll(s => s != thisPrime && s % thisPrime == 0);
+			}
+
+			return sieve;
 		}
 	}
 }
